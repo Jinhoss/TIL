85d459f0eb919b3902a1fc4c41c6c8a96fffcf0c
lalua456 <lalua456@gmail.com>
2021-02-05 12:08:26 +0900

    0205 추가

---
+++ programmers/완주하지 못한 선수.py
@@ -0,0 +1,7 @@
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for part,comp in zip(participant,completion) :
+        if part != comp :
+            return(part)
+    return participant[-1]
