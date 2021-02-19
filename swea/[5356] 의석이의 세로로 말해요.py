1f71b9bec78501218ed6f65c70f90f377f19505a
lalua456 <lalua456@gmail.com>
2021-02-19 13:23:31 +0900

    0219 string

---
+++ swea/[5356] 의석이의 세로로 말해요.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open("input.txt")
+
+T = int(input())
+
+for tc in range(1, T+1):
+    arr = [list(input()) for _ in range(5)]
+    result=''
+    for j in range(15):
+        for i in range(5):
+            if j<len(arr[i]):
+                result+=arr[i][j]
+    print('#{} {}'.format(tc,result))
+
