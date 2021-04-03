cd1a0492680473245dee6d160e2067a18d3b130a
lalua456 <lalua456@gmail.com>
2021-04-04 04:29:01 +0900

    0404

---
+++ programmers/2021_kakao_blind/메뉴 리뉴얼.py
@@ -0,0 +1,21 @@
+from itertools import combinations
+from collections import Counter
+
+
+def solution(orders, course):
+    answer = []
+    for c in course:
+        temp = []
+        for order in orders:
+            combi = combinations(sorted(order), c)
+            temp += combi
+        counter = Counter(temp)
+        if len(counter) != 0 and max(counter.values()) != 1:
+            answer += [''.join(f) for f in counter if counter[f] == max(counter.values())]
+
+    return sorted(answer)
+
+if __name__ == '__main__':
+    orders = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]
+    course = [2, 3, 4]
+    print(solution(orders, course))
