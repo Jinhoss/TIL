f33c930d0b3fd2f36db77766ce8d1165d5c487d1
lalua456 <lalua456@gmail.com>
2021-04-11 22:43:51 +0900

    0411

--- programmers/2018_kakao_blind/뉴스 클러스터링.py
+++ programmers/2018_kakao_blind/뉴스 클러스터링.py
@@ -1,25 +1,30 @@
 import re
-from collections import Counter as cs
-p = re.compile("[a-z]{2}")
-def multiSet(str):
+from collections import Counter
+
+def multiSet(string):
     lst = []
-    for idx in range(len(str)-1):
-        if p.match(str[idx:idx+2]):
-            lst.append(str[idx:idx+2])
+    # 두 글자씩 체크하며 다중집합 생성
+    for idx in range(len(string)-1):
+        if re.match('[a-z]{2}', string[idx:idx+2]):
+            lst.append(string[idx:idx+2])
     return lst
+
 def solution(str1, str2):
+    # 소문자 변환
     str1=str1.lower()
     str2=str2.lower()
-    re_x=multiSet(str1)
-    re_y=multiSet(str2)
-    set_x=cs(re_x)
-    set_y=cs(re_y)
-    int_set=list((set_x&set_y).elements())
+    # 다중집합 생성
+    multi_x = multiSet(str1)
+    multi_y = multiSet(str2)
+    # 다중집합은 중복을 허용함, Counter.elements를 이용하여 intersection
+    set_x = Counter(multi_x)
+    set_y = Counter(multi_y)
+    int_set = list((set_x & set_y).elements())
     
-    len_int=len(int_set)
-    len_union=len(re_x)+len(re_y)-len_int
-    if len(set_x)==0 and len(set_y)==0:
+    len_int = len(int_set)
+    len_union = len(multi_x)+len(multi_y) - len_int
+    if len(set_x) == 0 and len(set_y) == 0:
         return 65536
-    if len_union==0:
+    if len_union == 0:
         return 65536
-    return int(len_int/len_union*65536)
+    return int(len_int/len_union * 65536)
--- programmers/2018_kakao_blind/추석 트래픽.py
+++ programmers/2018_kakao_blind/추석 트래픽.py
@@ -1,21 +1,23 @@
-def time2durationSec (time):
+def time_transform(time):
     lst = time.split(' ')
     end = lst[1]
     duration = lst[2]
     
     lst2= end.split(':')
-    hour = int(lst2[0])*1000
-    minu = int(lst2[1])*1000
+    # 시간이 소수 셋째 자리까지 표현이 되어있으므로 1000을 곱해서 정수로 표현
+    hour = int(lst2[0]) * 1000
+    minute = int(lst2[1]) * 1000
     sec = int(lst2[2][0:2] + lst2[2][3:])
-    micsec = hour*3600 + minu*60 + sec
+    micsec = hour * 3600 + minute * 60 + sec
     lst3 = duration[:-1].split('.')
-    if len(lst3)>1:
-        duration2 = int(lst3[0])*1000 + int(lst3[1]+('0'*(3-len(lst3[1]))))
+    if len(lst3) > 1:
+        duration2 = int(lst3[0]) * 1000 + int(lst3[1].ljust(3, '0'))
     else: 
-        duration2 = int(lst3[0])*1000
+        duration2 = int(lst3[0]) * 1000
     
-    return [micsec-duration2 + 1, micsec]
-def checkProcessNum (time, lst):
+    return [micsec - duration2 + 1, micsec]
+# 로그의 처음과 끝 시간과 비교하여 count
+def checkProcessNum(time, lst):
     num = 0
     start = time
     end = time + 1000
@@ -23,15 +25,15 @@ def checkProcessNum (time, lst):
         if not (duration[1] < start or duration[0] >= end):
             num += 1
     return num
+
 def solution(lines):
-    answer = 0
     lst = []
     count = []
+    # 시간을 변환한 후 lst에 담음
     for string in lines:
-        lst.append(time2durationSec(string))
-    
-    for i in lst:
-    
+        lst.append(time_transform(string))
+    # 각 시간대마다 갯수를 count에 담음
+    for i in lst:    
         count.append(checkProcessNum(i[0], lst))
         count.append(checkProcessNum(i[1], lst))
     return max(count)
